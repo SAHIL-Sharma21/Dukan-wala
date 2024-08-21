617c280bb39aeba0760ef9c537d9dab18d1b9fba
Sahil <sahilsharma2000dtc@gmail.com>
2024-08-21 22:42:47 +0530

    e-comm project and files initiliszed

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import NavBar from './components/NavBar'
+
+function App() {
+ 
+
+  return (
+    <>
+      <nav>
+        <NavBar />
+      </nav>
+      <div className='flex items-center justify-center min-h-screen'>
+        <main>
+          <h2>all the products are going to render here!!</h2>
+        </main>
+      </div>
+    </>
+  )
+}
+
+export default App
